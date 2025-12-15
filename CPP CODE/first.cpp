ad2b0a263b9e025fff057e32cb1572026d842b5f
Qasim Haleem <mrqasimhaleem@gmail.com>
2025-12-15 11:46:49 -0800

    setting done

---
+++ CPP CODE/first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, VS Code!" << endl;
+    cout << "My name is Muhammad Qasimm" << endl;
+    cout << "a studen!" << endl;
+
+    return 0;
+}
