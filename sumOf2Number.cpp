825fe0836587b9fee27d3ebfd66a8e2eebb861fc
Qasim Haleem <mrqasimhaleem@gmail.com>
2026-01-10 22:08:19 -0800

    Basics of C++ for the DSA

---
+++ sumOf2Number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Please enter the value of A" << endl;
+    cin >> a;
+    cout << "Please enter the value of B" << endl;
+    cin >> b;
+
+    cout << "Sum is : " << a + b;
+}
---
+++ typeCasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double num = 10.9999;
+    cout << "the double value are the " << num << endl;
+    int newNum = (int)num;
+    cout << "after type casting  " << newNum << endl;
+}
